db60bf9da492b58803071cf215ebba10425b3f84
kristyldatical <kgomes@datical.com>
2018-06-26 16:17:18 -0500

    DDL #4

---
+++ sql_code/ddl/create_table_4.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [SOURCE_0626__CAT_1].[Schema_A].[Users](
+    [User_Id] [int] NOT NULL,
+    [LastName] [varchar](255) NOT NULL,
+    [FirstName] [varchar](255) NULL,
+    [Address] [varchar](255) NULL,
+    [City] [varchar](255) NULL
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
