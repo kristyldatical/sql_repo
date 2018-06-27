737647aed457997bafce787d007eb872d2896e6d
kristyldatical <kgomes@datical.com>
2018-06-27 14:09:48 -0500

    committing

---
+++ sql_code/ddl/create_table_CAT2-S2-ddl-fully-qualified-noMS.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [SOURCE_0626__CAT_2].[Schema_2].[Users_Cat2_retest_schema2](
+    [User_Id] [int] NOT NULL,
+    [LastName] [varchar](255) NOT NULL,
+    [FirstName] [varchar](255) NULL,
+    [Address] [varchar](255) NULL,
+    [City] [varchar](255) NULL
+) ON [PRIMARY]z
