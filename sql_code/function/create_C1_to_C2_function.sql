c15c8b2e80c5b0084ee95265dd0b2f52c6348000
kristyldatical <kgomes@datical.com>
2018-06-27 15:38:16 -0500

    committing function

---
+++ sql_code/function/create_C1_to_C2_function.sql
@@ -0,0 +1,11 @@
+-- function in SOURCE_0626__CAT_1 (metadata.properties), schema A referring to table 2 in SOURCE_0626__CAT_2
+USE [SOURCE_0626__CAT_1]
+GO
+	
+CREATE FUNCTION "Schema_A"."COUNT_FUNC"()
+  RETURNS int AS 
+  BEGIN
+    DECLARE @ct int
+    select @ct=count(*) from [SOURCE_0626__CAT_2].[Schema_2].[Users]
+    RETURN @ct
+  END;
