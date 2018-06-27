c4dd38e55a1d2042559062eacf77abca9d73cfc1
kristyldatical <kgomes@datical.com>
2018-06-27 13:27:01 -0500

    committing

---
+++ sql_code/ddl/create_table_CAT1-SB-fully-qualified.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [SOURCE_0626__CAT_1].[Schema_B].[Users_Cat1_SB](
+    [User_Id] [int] NOT NULL,
+    [LastName] [varchar](255) NOT NULL,
+    [FirstName] [varchar](255) NULL,
+    [Address] [varchar](255) NULL,
+    [City] [varchar](255) NULL
+) ON [PRIMARY]
