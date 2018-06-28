b8e77171f823d4f7a5cbc513113bfd0d15778d45
kristyldatical <kgomes@datical.com>
2018-06-28 11:31:40 -0500

    committing

--- sql_code/function/create_C1_to_C2_function - Fully qualified name.sql
+++ sql_code/function/create_C1_to_C2_function - Fully qualified name.sql
@@ -1,7 +1,7 @@
 -- function in SOURCE_0626__CAT_1 (metadata.properties), schema A referring to table 2 in SOURCE_0626__CAT_2
 -- Fully qualified name
 	
-CREATE FUNCTION "SOURCE_0626__CAT_1"."SCHEMA_A"."COUNT_FUNC_TWO"()
+CREATE FUNCTION "SCHEMA_A"."COUNT_FUNC_TWO"()
   RETURNS int AS 
   BEGIN
     DECLARE @ct int
