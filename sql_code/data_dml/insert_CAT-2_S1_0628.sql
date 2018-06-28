38ef6488cec4051d26ec8f9cd41a40c6d6b6500a
kristyldatical <kgomes@datical.com>
2018-06-28 16:11:41 -0500

    committing [commit-label-test-0628-1]

--- sql_code/data_dml/insert_CAT-2_S1_0628.sql
+++ sql_code/data_dml/insert_CAT-2_S1_0628.sql
@@ -1,11 +1,5 @@
 INSERT INTO [SOURCE_0626__CAT_2].[Schema_1].[Users_Cat2_S1] (User_Id, LastName, FirstName, Address, City)
-VALUES (1, '3333', 'DDGDFD', 'ADA', 'SQWE');
-VALUES (1, '44', 'TYUTYU', 'SDF', 'HJKHJK');
-VALUES (1, '5', 'RETRET', 'TYTYU', 'HJKH');
-VALUES (1, '6', 'GJHGJ', 'SDSF', 'HJKHJKH');
+VALUES (1, '3333', 'DDGDFD', 'ADA', 'SQWE'),(1, '44', 'TYUTYU', 'SDF', 'HJKHJK'),(1, '5', 'RETRET', 'TYTYU', 'HJKH'),(1, '6', 'GJHGJ', 'SDSF', 'HJKHJKH');
 
 INSERT INTO [SOURCE_0626__CAT_2].[Schema_2].[Users_Cat2_S2] (User_Id, LastName, FirstName, Address, City)
-VALUES (1, '12', 'SASD', 'IY', 'KJJKL');
-VALUES (1, '6656', 'WED', 'UI', 'ERW');
-VALUES (1, '657', 'GFG', 'CVB', 'BVN');
-VALUES (1, '52342', 'UYUY', 'JKL', 'VBNBV');
+VALUES (1, '12', 'SASD', 'IY', 'KJJKL'),(1, '6656', 'WED', 'UI', 'ERW'),(1, '657', 'GFG', 'CVB', 'BVN'),(1, '52342', 'UYUY', 'JKL', 'VBNBV');
