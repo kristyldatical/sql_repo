8c654b72475c6ea8e0c8bfd46e6269114b7abdf0
kristyldatical <kgomes@datical.com>
2018-06-28 16:15:16 -0500

    committing [KG-label-test-0628-2]

---
+++ sql_code/data_dml/insert_CAT-1_0628.sql
@@ -0,0 +1,5 @@
+INSERT INTO [SOURCE_0626__CAT_2].[Schema_1].[Users_Cat2_S1] (User_Id, LastName, FirstName, Address, City)
+VALUES (1, '66', 'DDGDFD', 'ADA', 'SQWE'),(1, '456', 'TYUTYU', 'SDF', 'HJKHJK'),(1, '45654', 'RETRET', 'TYTYU', 'HJKH'),(1, '2323', 'GJHGJ', 'SDSF', 'HJKHJKH');
+
+INSERT INTO [SOURCE_0626__CAT_2].[Schema_2].[Users_Cat2_S2] (User_Id, LastName, FirstName, Address, City)
+VALUES (1, '4345', 'SASD', 'IY', 'KJJKL'),(1, '12', 'WED', 'UI', 'ERW'),(1, '65734', 'GFG', 'CVB', 'BVN'),(1, '562', 'UYUY', 'JKL', 'VBNBV');
