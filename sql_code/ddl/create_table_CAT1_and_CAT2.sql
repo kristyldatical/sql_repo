7a448c7b8ded826ee551273fa9f872d4c52cd067
kristyldatical <kgomes@datical.com>
2018-06-28 12:24:30 -0500

    committing

---
+++ sql_code/ddl/create_table_CAT1_and_CAT2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [SOURCE_0626__CAT_1].[Schema_A].[Users_628](
+    [User_Id] [int] NOT NULL,
+    [LastName] [varchar](255) NOT NULL,
+    [FirstName] [varchar](255) NULL,
+    [Address] [varchar](255) NULL,
+    [City] [varchar](255) NULL
+) ON [PRIMARY]
+
+CREATE TABLE [SOURCE_0626__CAT_2].[Schema_1].[Users_628](
+    [User_Id] [int] NOT NULL,
+    [LastName] [varchar](255) NOT NULL,
+    [FirstName] [varchar](255) NULL,
+    [Address] [varchar](255) NULL,
+    [City] [varchar](255) NULL
+) ON [PRIMARY]
