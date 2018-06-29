8e2c6386f52bcf156766ca9d8eeaef25e380a585
kristyldatical <kgomes@datical.com>
2018-06-29 12:17:28 -0500

    Test Views [commit view label-2]

--- sql_code/view/mssql_cross-catalog-view.sql
+++ sql_code/view/mssql_cross-catalog-view.sql
@@ -2,5 +2,5 @@
 
 CREATE VIEW [SCHEMA_B].[VIEW_0629] AS
 SELECT FirstName, LastName
-FROM [SOURCE_0626__CAT_2].[Schema_2].[Users_Cat2_S2];
+FROM [SOURCE_0626__CAT_2].[Schema_2].[Users_Cat2_S2]
 WHERE FirstName='WED';
