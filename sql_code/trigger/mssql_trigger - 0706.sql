ea9a40c31852e1cf974f9e9e3b3906dae9bcf285
kristyldatical <kgomes@datical.com>
2018-07-06 13:17:25 -0500

    committing

--- sql_code/trigger/mssql_trigger - 0706.sql
+++ sql_code/trigger/mssql_trigger - 0706.sql
@@ -1,6 +1,6 @@
 -- Create trigger  
-CREATE TRIGGER [SCHEMA_1].[TRIGGER_0706]
-   ON  [SCHEMA_1].[Users_0628]
+CREATE TRIGGER [SCHEMA_1].[TRIGGER_07066_1]
+   ON  [SCHEMA_1].[Users_Cat2_S1]
 	FOR INSERT
 AS 
 	-- other comment
@@ -11,7 +11,7 @@ BEGIN
 	SET NOCOUNT ON
 
 	SELECT * FROM 
-    [SOURCE_0626__CAT_1].[Schema_A].[Users_0629];
+    [SOURCE_0626__CAT_1].[Schema_A].[Users_Cat1_SA];
      PRINT 'Done';
 
 END
