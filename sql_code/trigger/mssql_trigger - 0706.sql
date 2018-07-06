efe344e36bdc2cef549bbaebce9b50b14a19a063
kristyldatical <kgomes@datical.com>
2018-07-06 13:06:54 -0500

    committing

---
+++ sql_code/trigger/mssql_trigger - 0706.sql
@@ -0,0 +1,18 @@
+-- Create trigger  
+CREATE TRIGGER [TRIGGER_0706]
+   ON  [Users_0628]
+	FOR INSERT
+AS 
+	-- other comment
+    PRINT N'Hello, World! I am an MSSQL trigger.'
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON
+
+	SELECT * FROM 
+    [SOURCE_0626__CAT_1].[Schema_A].[Users_0629];
+     PRINT 'Done';
+
+END
+;
